b0a659b93b14a7fa55f131dee26d06485345dd40
cuijingjing <1262120369@qq.com>
2016-12-29 16:06:09 +0800

    export module

--- src/main.js
+++ src/main.js
@@ -2,85 +2,11 @@ import Vue from "vue";
 import VueRouter from "vue-router";
 import styles from './index.scss';         //导入css样式表
 //import './index.css';         //导入css样式表
+//import dpr from "./plugin/dpr";    //不加括号相当于为整个模块起了一个别名，加括号直接引用模块内的方法/对象
+//dpr.dpr();
+import {dpr} from "./plugin/dpr";
+dpr();
 Vue.use(VueRouter);
-;(function(win) {
-  var doc = win.document;
-  var docEl = doc.documentElement;
-  var metaEl = doc.querySelector('meta[name="viewport"]');
-  var originHeight;
-  var dpr = 0;
-  var scale = 0;
-  var tid;
-  var flag = 1;
-  if (!dpr && !scale) {
-    var isAndroid = win.navigator.appVersion.match(/android/gi);
-    var isIPhone = win.navigator.appVersion.match(/iphone/gi);
-    var devicePixelRatio = win.devicePixelRatio;
-    if (isIPhone) {
-      // iOS下，对于2和3的屏，用2倍的方案，其余的用1倍方案
-      if (devicePixelRatio >= 3 && (!dpr || dpr >= 3)) {
-        dpr = 3;
-      } else if (devicePixelRatio >= 2 && (!dpr || dpr >= 2)){
-        dpr = 2;
-      } else {
-        dpr = 1;
-      }
-    } else {
-      // 其他设备下，仍旧使用1倍的方案
-      dpr = 1;
-    }
-    scale = 1 / dpr;
-  }
-  docEl.setAttribute('data-dpr', dpr);
-  //如果设置了viewport，则根据默认设置进行显示
-  if (metaEl) {
-    console.warn('将根据已有的meta标签来设置缩放比例');
-    var match = metaEl.getAttribute('content').match(/initial\-scale=([\d\.]+)/);
-    if (match) {
-      scale = parseFloat(match[1]);
-      dpr = parseInt(1 / scale);
-    }
-  } else {
-    metaEl = doc.createElement('meta');
-    metaEl.setAttribute('name', 'viewport');
-    metaEl.setAttribute('content', 'initial-scale=' + scale + ', maximum-scale=' + scale + ', minimum-scale=' + scale + ', user-scalable=no');
-    if (docEl.firstElementChild) {
-      docEl.firstElementChild.appendChild(metaEl);
-    } else {
-      var wrap = doc.createElement('div');
-      wrap.appendChild(metaEl);
-      doc.write(wrap.innerHTML);
-    }
-  }
-  function refreshRem(){
-    if(flag){
-      originHeight = document.documentElement.clientHeight;
-      docEl.style.height = originHeight + "px";
-      console.log(document.getElementsByTagName("html"))
-      flag = 0;
-    }
-    var width = docEl.getBoundingClientRect().width;
-    //var height = docEl.getBoundingClientRect().height;
-    var rem = width / 10;
-    docEl.style.fontSize = rem + 'px';
-  }
-  win.addEventListener('resize', function() {
-    clearTimeout(tid);
-    tid = setTimeout(refreshRem, 300);
-  }, false);
-
-  if (doc.readyState === 'complete') {
-    doc.body.style.fontSize = 12 * dpr + 'px';
-  } else {
-    doc.addEventListener('DOMContentLoaded', function(e) {
-      doc.body.style.fontSize = 12 * dpr + 'px';
-    }, false);
-  }
-  refreshRem();
-  //alert(window.innerWidth);
-  //alert(docEl.getBoundingClientRect().width);
-})(window);
-
 var App = Vue.extend({})
 var router = new VueRouter()
 router.map({
@@ -123,5 +49,4 @@ router.map({
   }
 
 })
-
 router.start(App, '#app')
